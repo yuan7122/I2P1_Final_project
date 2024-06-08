5881d18f5c93bf7a0192fcc4b3e06cf5d17f8cc7
lintzoe <lintzoe@gmail.com>
2024-06-08 12:45:56 +0800

    strawberry appears and seeds_c and seeds_e file c and h are founed

--- Code/element/seeds_c.c
+++ Code/element/seeds_c.c
@@ -1,16 +1,16 @@
-#include "seeds_s.h"
+#include "seeds_c.h"
 #include "../shapes/Circle.h"
 /*
-   [Seeds_s function]
+   [Seeds_c function]
 */
 //deleted int v. by lintzoe
-Elements *New_Seeds_s(int label, int x, int y)
+Elements *New_Seeds_c(int label, int x, int y)
 {
-    Seeds_s *pDerivedObj = (Seeds_s *)malloc(sizeof(Seeds_s));
+    Seeds_c *pDerivedObj = (Seeds_c *)malloc(sizeof(Seeds_c));
     Elements *pObj = New_Elements(label);
     // setting derived object member
     
-    pDerivedObj->img = al_load_bitmap("assets/image/seeds_s.png");
+    pDerivedObj->img = al_load_bitmap("assets/image/seeds_c.png");
     pDerivedObj->width = al_get_bitmap_width(pDerivedObj->img);
     pDerivedObj->height = al_get_bitmap_height(pDerivedObj->img);
     pDerivedObj->x = x;
@@ -24,30 +24,30 @@ Elements *New_Seeds_s(int label, int x, int y)
     pObj->inter_obj[pObj->inter_len++] = Floor_L;*/
     // setting derived object function
     pObj->pDerivedObj = pDerivedObj;
-    pObj->Update = Seeds_s_update;
-    pObj->Interact = Seeds_s_interact;
-    pObj->Draw = Seeds_s_draw;
-    pObj->Destroy = Seeds_s_destory;
+    pObj->Update = Seeds_c_update;
+    pObj->Interact = Seeds_c_interact;
+    pObj->Draw = Seeds_c_draw;
+    pObj->Destroy = Seeds_c_destory;
 
     return pObj;
 }
-void Seeds_s_update(Elements *self)
+void Seeds_c_update(Elements *self)
 {
-    Seeds_s *Obj = ((Seeds_s *)(self->pDerivedObj));
-    _Seeds_s_update_position(self, Obj->v, 0);
+    Seeds_c *Obj = ((Seeds_c *)(self->pDerivedObj));
+    _Seeds_c_update_position(self, Obj->v, 0);
 }
-void _Seeds_s_update_position(Elements *self, int dx, int dy)
+void _Seeds_c_update_position(Elements *self, int dx, int dy)
 {
-    /*Seeds_s *Obj = ((Seeds_s *)(self->pDerivedObj));
+    /*Seeds_c *Obj = ((Seeds_c *)(self->pDerivedObj));
     Obj->x += dx;
     Obj->y += dy;
     Shape *hitbox = Obj->hitbox;
     hitbox->update_center_x(hitbox, dx);
     hitbox->update_center_y(hitbox, dy);*/
 }
-void Seeds_s_interact(Elements *self, Elements *tar)
+void Seeds_c_interact(Elements *self, Elements *tar)
 {
-    //Seeds_s *Obj = ((Seeds_s *)(self->pDerivedObj));
+    //Seeds_c *Obj = ((Seeds_c *)(self->pDerivedObj));
     /*if (tar->label == Floor_L)
     {
         if (Obj->x < 0 - Obj->width)
@@ -64,17 +64,17 @@ void Seeds_s_interact(Elements *self, Elements *tar)
         }
     }*/
 }
-void Seeds_s_draw(Elements *self)
+void Seeds_c_draw(Elements *self)
 {
-    Seeds_s *Obj = ((Seeds_s *)(self->pDerivedObj));
+    Seeds_c *Obj = ((Seeds_c *)(self->pDerivedObj));
     if (Obj->v > 0)
         al_draw_bitmap(Obj->img, Obj->x, Obj->y, ALLEGRO_FLIP_HORIZONTAL);
     else
         al_draw_bitmap(Obj->img, Obj->x, Obj->y, 0);
 }
-void Seeds_s_destory(Elements *self)
+void Seeds_c_destory(Elements *self)
 {
-    Seeds_s *Obj = ((Seeds_s *)(self->pDerivedObj));
+    Seeds_c *Obj = ((Seeds_c *)(self->pDerivedObj));
     al_destroy_bitmap(Obj->img);
     free(Obj->hitbox);
     free(Obj);
